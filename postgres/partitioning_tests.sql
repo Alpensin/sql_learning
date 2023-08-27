7e594c8a9225b5b75c64a555c85dd1435df0b80c
Anton Mardanov <Lotrus@yandex.ru>
2023-08-27 16:46:41 +0300

    Create partitioning_tests.sql

---
+++ postgres/partitioning_tests.sql
@@ -0,0 +1,89 @@
+-- POSTGRES 12.
+--- TEST1. Чистые таблицы
+CREATE TABLE test_table (
+    a INTEGER,
+    b INTEGER,
+    c INTEGER
+) PARTITION BY LIST (c);
+
+CREATE TABLE test_table_1 PARTITION OF test_table FOR VALUES IN (1);
+CREATE TABLE test_table_2 PARTITION OF test_table FOR VALUES IN (2);
+CREATE TABLE test_table_3 PARTITION OF test_table FOR VALUES IN (3);
+
+DROP TABLE test_table_1;
+
+INSERT INTO test_table (a, b, c) SELECT generate_series(1, 500), generate_series(1, 500), 1 FROM generate_series(1, 5000);
+/* После запуска, пока идет вставка, вставлял данные через другой клиент в другие секции. И делал запрос на просмотр данных
+INSERT INTO test_table (a, b, c) SELECT generate_series(1, 50), generate_series(1, 50), 2 FROM generate_series(1, 50);
+INSERT INTO test_table (a, b, c) SELECT generate_series(1, 50), generate_series(1, 50), 3 FROM generate_series(1, 50);
+select c, count(c) from test_table group by c;
+НЕТ БЛОКИРОВОК других партиций при вставке
+*/
+
+--- TEST2 - есть PK (a, c)
+
+DROP TABLE test_table;
+
+CREATE TABLE test_table (
+    a INTEGER,
+    b INTEGER,
+    c INTEGER,
+    PRIMARY KEY(a, c)
+) PARTITION BY LIST (c);
+CREATE TABLE test_table_1 PARTITION OF test_table FOR VALUES IN (1);
+CREATE TABLE test_table_2 PARTITION OF test_table FOR VALUES IN (2);
+CREATE TABLE test_table_3 PARTITION OF test_table FOR VALUES IN (3);
+INSERT INTO test_table (a, b, c) SELECT generate_series(1, 1000000), 444, 1;
+/* После запуска, пока идет вставка, вставлял данные через другой клиент в другие секции. И делал запрос на просмотр данных
+INSERT INTO test_table (a, b, c) SELECT generate_series(1, 50), 444, 2;
+INSERT INTO test_table (a, b, c) SELECT generate_series(1, 50), 444, 3;
+select c, count(c) from test_table group by c;
+НЕТ БЛОКИРОВОК других партиций при вставке
+*/
+
+
+--- TEST3 - есть PK (a, c) и есть общий sequence
+
+DROP TABLE test_table;
+DROP SEQUENCE IF EXISTS test_table_a_seq_1;
+CREATE SEQUENCE test_table_a_seq_1 START 1;
+CREATE TABLE test_table (
+    a INTEGER DEFAULT nextval('test_table_a_seq_1'),
+    b INTEGER,
+    c INTEGER,
+    PRIMARY KEY(a, c)
+) PARTITION BY LIST (c);
+CREATE TABLE test_table_1 PARTITION OF test_table FOR VALUES IN (1);
+CREATE TABLE test_table_2 PARTITION OF test_table FOR VALUES IN (2);
+CREATE TABLE test_table_3 PARTITION OF test_table FOR VALUES IN (3);
+INSERT INTO test_table (b, c) SELECT generate_series(1, 1000000), 1;
+/* После запуска, пока идет вставка, вставлял данные через другой клиент в другие секции. И делал запрос на просмотр данных
+INSERT INTO test_table (b, c) SELECT generate_series(1, 100), 2;
+INSERT INTO test_table (b, c) SELECT generate_series(1, 100), 3;
+select c, count(c) from test_table group by c;
+НЕТ БЛОКИРОВОК других партиций при вставке. Общий ключ отрабатывает корректно.
+*/
+
+--- TEST4 - есть PK (a, c) и есть общий sequence. Есть общий индекс
+
+DROP TABLE test_table;
+DROP SEQUENCE IF EXISTS test_table_a_seq_1;
+CREATE SEQUENCE test_table_a_seq_1 START 1;
+CREATE TABLE test_table (
+    a INTEGER DEFAULT nextval('test_table_a_seq_1'),
+    b INTEGER NOT NULL,
+    c INTEGER,
+    d INTEGER NOT NULL,
+    PRIMARY KEY(a, c)
+) PARTITION BY LIST (c);
+CREATE INDEX test_table_b_d_index ON test_table (b, d);
+CREATE TABLE test_table_1 PARTITION OF test_table FOR VALUES IN (1);
+CREATE TABLE test_table_2 PARTITION OF test_table FOR VALUES IN (2);
+CREATE TABLE test_table_3 PARTITION OF test_table FOR VALUES IN (3);
+INSERT INTO test_table (b, c, d) SELECT generate_series(1, 1000000), 1, 50;
+/* После запуска, пока идет вставка, вставлял данные через другой клиент в другие секции. И делал запрос на просмотр данных
+INSERT INTO test_table (b, c, d) SELECT generate_series(1, 100), 2, 60;
+INSERT INTO test_table (b, c, d) SELECT generate_series(1, 100), 3, 70;
+select c, count(c) from test_table group by c;
+НЕТ БЛОКИРОВОК других партиций при вставке. Общий ключ отрабатывает корректно.
+*/
